3a230af7f74f0c8c03c6f5c85f7d63056255b253
Phil <46725438+Tim8s@users.noreply.github.com>
2019-01-16 09:56:30 -0500

    Creation Scene Menu

---
+++ Taverne/Assets/CuisinierMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CuisinierMenu : MonoBehaviour {
+
+    Scene scene;
+
+	// Use this for initialization
+	void Start () {
+        scene = SceneManager.GetActiveScene();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (scene.name == "Scene Menu")
+        {
+            print("YAY");
+        }
+	}
+}
